d98b24aa4cecfd16cd8f8b90e4a1e074785dfe31
Emaliana Kasmuri <emaliana.kasmuri@gmail.com>
2021-04-30 13:20:45 +0800

    Adding comments

--- multithreads/src/demo/sleep/MultiThreadSleepDemo.java
+++ multithreads/src/demo/sleep/MultiThreadSleepDemo.java
@@ -1,9 +1,16 @@
 package demo.sleep;
 
+/**
+ * This class demonstrate suspension of threads.
+ * 
+ * @author emalianakasmuri
+ *
+ */
 public class MultiThreadSleepDemo {
 
 	public static void main(String[] args) {
 		
+		// Create objects of thread and give names
 		Runnable numberable = new NumberPrinter();
 		Thread numberThread = new Thread(numberable);
 		numberThread.setName("numberThread");
@@ -12,7 +19,7 @@ public class MultiThreadSleepDemo {
 		statementThread.setName("statementThread");
 		
 		
-		
+		// Execute thread
 		statementThread.start();
 		numberThread.start();
 
--- multithreads/src/demo/sleep/StatementPrinter.java
+++ multithreads/src/demo/sleep/StatementPrinter.java
@@ -1,9 +1,15 @@
 package demo.sleep;
 
+/**
+ * This class represents a Printer to print text.
+ * @author emalianakasmuri
+ *
+ */
 public class StatementPrinter extends Thread {
 
 	/**
 	 * This method prints a statement for five times
+	 * and demonstrate suspension of Thread for 1 second
 	 */
 	public void printStatement() {
 
@@ -11,8 +17,11 @@ public class StatementPrinter extends Thread {
 			for (int counter = 0; counter < 5; counter++) {
 	
 				System.out.println("Java is easy");
+				
+				// Suspend the current thread for 1000 milliseconnds
 				Thread.sleep(1000);
 			}
+			
 		} catch (Exception ex) {
 			
 			ex.printStackTrace();
@@ -21,7 +30,8 @@ public class StatementPrinter extends Thread {
 
 	@Override
 	public void run() {
-
+		
+		// Execute task
 		printStatement();
 
 	}
