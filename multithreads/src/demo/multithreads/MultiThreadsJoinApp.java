1e3608b42a7eb1904d68bdbcd9e54fc261eb6fd5
Emaliana Kasmuri <emaliana.kasmuri@gmail.com>
2021-04-30 13:32:11 +0800

    Adding comments

--- multithreads/src/demo/multithreads/MultiThreadsJoinApp.java
+++ multithreads/src/demo/multithreads/MultiThreadsJoinApp.java
@@ -3,10 +3,19 @@ package demo.multithreads;
 import demo.extthr.StatementPrinter;
 import demo.imprun.NumberPrinter;
 
+/**
+ * This class is used to differentiate between joining and non-joining threads.
+ * 
+ * This is a class with joining the threads.
+ * 
+ * @author emalianakasmuri
+ *
+ */
 public class MultiThreadsJoinApp {
 
 	public static void main(String[] args) {
-				
+		
+		// Create object of threads
 		Thread statementThread = new StatementPrinter();
 		
 		Runnable numberable1 = new NumberPrinter();
@@ -15,11 +24,14 @@ public class MultiThreadsJoinApp {
 		Thread numberThread2 = new Thread(numberable1, "Number 2");
 		
 		try {
-		
+			
+			// Execute the threads
 			numberThread2.start();
 			statementThread.start();
 			numberThread1.start();
 			
+			// Let threads of lower priority to execute,
+			// wait until its finished, then continue execution
 			numberThread2.join();
 			statementThread.join();
 			numberThread1.join();
--- multithreads/src/demo/multithreads/MultiThreadsWoutJoinApp.java
+++ multithreads/src/demo/multithreads/MultiThreadsWoutJoinApp.java
@@ -3,10 +3,19 @@ package demo.multithreads;
 import demo.extthr.StatementPrinter;
 import demo.imprun.NumberPrinter;
 
+/**
+ * This class is used to differentiate between joining and non-joining threads.
+ * 
+ * This is a class without joining the threads.
+ * 
+ * @author emalianakasmuri
+ *
+ */
 public class MultiThreadsWoutJoinApp {
 
 	public static void main(String[] args) {
 				
+		// Create object of threads
 		Thread statementThread = new StatementPrinter();
 		
 		Runnable numberable1 = new NumberPrinter();
@@ -16,10 +25,13 @@ public class MultiThreadsWoutJoinApp {
 		
 		try {
 		
+			// Execute threads
 			numberThread2.start();
 			statementThread.start();
 			numberThread1.start();
 			
+			// To indicate program has ends. 
+			// It should printed after all threads has finished/died
 			System.out.println("Program ends");
 		
 		} catch (Exception ex) {
