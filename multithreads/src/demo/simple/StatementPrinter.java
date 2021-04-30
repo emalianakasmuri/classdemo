2a9daa0fb48aef78c0444ab742206243866df360
Emaliana Kasmuri <emaliana.kasmuri@gmail.com>
2021-04-30 12:24:06 +0800

    First commit.

---
+++ multithreads/src/demo/simple/StatementPrinter.java
@@ -0,0 +1,16 @@
+package demo.simple;
+
+public class StatementPrinter {
+	
+	public void printStatement () {
+		
+		for (int counter = 0; counter < 5; counter++) {
+
+			System.out.println("Java is easy");
+		}
+	}
+
+}
+
+
+
