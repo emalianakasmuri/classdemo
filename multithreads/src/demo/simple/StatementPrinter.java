7e6f571616f4b33b493f9c5d156adff3fceb1118
Emaliana Kasmuri <emaliana.kasmuri@gmail.com>
2021-04-30 12:58:51 +0800

    Second commit

--- multithreads/src/demo/simple/StatementPrinter.java
+++ multithreads/src/demo/simple/StatementPrinter.java
@@ -1,7 +1,18 @@
 package demo.simple;
 
+/**
+ * This class represents a task for a printer
+ * 
+ * @author emalianakasmuri
+ *
+ */
 public class StatementPrinter {
 	
+	/**
+	 * This method represent a task carried out by the printer.
+	 * It prints a statement based on the number of the counter
+	 * 
+	 */
 	public void printStatement () {
 		
 		for (int counter = 0; counter < 5; counter++) {
---
+++ multithreads/src/demo/simple/StatementPrinterApp.java
@@ -0,0 +1,19 @@
+package demo.simple;
+
+
+/**
+ * This class represent execution of class StatementPrinter
+ * 
+ * @author emalianakasmuri
+ *
+ */
+public class StatementPrinterApp {
+
+	public static void main(String[] args) {
+		
+		StatementPrinter printer1 = new StatementPrinter();
+		printer1.printStatement();
+
+	}
+
+}
