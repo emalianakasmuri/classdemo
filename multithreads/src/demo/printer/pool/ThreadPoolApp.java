d35aff319e527b039ecfecca8adecd22de98bf97
Emaliana Kasmuri <emaliana.kasmuri@gmail.com>
2021-05-07 16:18:05 +0800

    New commit

---
+++ multithreads/src/demo/printer/pool/ThreadPoolApp.java
@@ -0,0 +1,41 @@
+package demo.printer.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import demo.imprun.NumberPrinter;
+
+/**
+ * This program demonstrate the implementation of a thread pool.
+ * 
+ * @author emalianakasmuri
+ *
+ */
+public class ThreadPoolApp {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// Create runnable objects
+		Runnable numberPrinter1 = new NumberPrinter();
+		Runnable numberPrinter2 = new NumberPrinter();
+		Runnable numberPrinter3 = new NumberPrinter();
+				
+		// Create a fixed thread pool with maximum three threads
+		ExecutorService printExecutorService = Executors.newFixedThreadPool(3);
+		
+		// Submit to executor
+		printExecutorService.execute(numberPrinter3);
+		printExecutorService.execute(numberPrinter1);
+		printExecutorService.execute(numberPrinter2);
+		
+		// Shutdown the executor
+		// No new task will be accepted, the existing will continue until finish
+		printExecutorService.shutdown();
+		
+		
+
+	}
+
+}
